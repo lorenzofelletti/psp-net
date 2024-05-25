d03bdc59dd926a74439ee38be191deb91cdf0457
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2024-05-25 18:54:18 +0100

    docs: improve documentation

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,6 +1,10 @@
 use crate::{traits::io::OptionType, SocketAddr};
 
-#[derive(Clone, Copy)]
+/// Socket options, such as remote address to connect to.
+/// This is used by [`TcpSocket`](super::socket::tcp::TcpSocket) and
+/// [`UdpSocket`](super::socket::udp::UdpSocket) when used as
+/// [`EasySocket`](super::traits::io::EasySocket)s.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct SocketOptions {
     pub remote: SocketAddr,
 }
@@ -10,30 +14,35 @@ impl OptionType for SocketOptions {
 }
 
 impl SocketOptions {
+    /// Create a new socket options
     pub fn new(remote: SocketAddr) -> SocketOptions {
         Self { remote }
     }
 
+    /// Get the remote address
     pub fn remote(&self) -> SocketAddr {
         self.remote
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct TlsSocketOptions {
     pub remote: SocketAddr,
     pub seed: u64,
 }
 
 impl TlsSocketOptions {
+    /// Create a new socket options
     pub fn new(remote: SocketAddr, seed: u64) -> Self {
         Self { remote, seed }
     }
 
+    /// Get the remote address
     pub fn remote(&self) -> SocketAddr {
         self.remote
     }
 
+    /// Get the seed
     pub fn seed(&self) -> u64 {
         self.seed
     }
