b03cfbd9e11da724759d9576d86eef5473bf4963
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2024-05-30 20:51:01 +0100

    feat: improve utils functions

--- src/utils.rs
+++ src/utils.rs
@@ -12,6 +12,8 @@ pub enum NetError {
     Error(String, i32),
     /// Net config Do not exist
     NetConfigNotExist,
+    /// Timeout
+    Timeout,
 }
 
 impl NetError {
@@ -123,4 +125,60 @@ pub fn check_netconfig_existence(id: i32) -> Result<(), NetError> {
 #[inline]
 pub fn check_first_netconfig_existence() -> Result<(), NetError> {
     check_netconfig_existence(1)
+}
+
+/// Initialise a connection to an access point
+///
+/// # Parameters
+/// - `connection_id`: The ID of the access point to connect to
+///
+/// # Errors
+/// [`NetError::Error`] if the connection initialisation failed
+///
+/// # Notes
+/// The access point ID is a number greater or equal to 1. It is the index of the
+/// access point in the netconfig.
+///
+/// This function does not block until the connection is established.
+/// Use [`block_until_connected`](crate::utils::block_until_connected) right after
+/// this function to block until the connection is established.
+pub fn init_connection_to_access_point(connection_id: i32) -> Result<(), NetError> {
+    unsafe {
+        let res = psp::sys::sceNetApctlConnect(connection_id);
+        if res != 0 {
+            return Err(NetError::error("sceNetApctlConnect", res));
+        }
+    }
+
+    Ok(())
+}
+
+/// Block until connected
+///
+/// Polls the access point state until connection is established, `desist_after` times
+/// is reached, or an error occurs.
+/// The polling is done at 50ms intervals using [`sceKernelDelayThread`](psp::sys::sceKernelDelayThread).
+///
+/// # Parameters
+/// - `desist_after`: The number of times to poll the state before desisting
+///
+/// # Errors
+/// Same as [`init_connection_to_access_point`](crate::utils::init_connection_to_access_point)
+#[inline]
+pub fn block_until_connected(desist_after: usize) -> Result<(), NetError> {
+    let mut state: psp::sys::ApctlState = unsafe { core::mem::zeroed() };
+    for _ in 0..desist_after {
+        unsafe {
+            let err = psp::sys::sceNetApctlGetState(&mut state);
+            if err != 0 {
+                return Err(NetError::error("sceNetApctlGetState", err));
+            }
+        }
+        if let psp::sys::ApctlState::GotIp = state {
+            return Ok(());
+        }
+        unsafe { psp::sys::sceKernelDelayThread(50_000) };
+    }
+
+    Err(NetError::Timeout)
 }
