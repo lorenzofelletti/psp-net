b00120bb83322e95c4c88437191d0aa312ebf2de
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2024-12-08 16:32:20 +0000

    wip

--- src/http/macros.rs
+++ src/http/macros.rs
@@ -5,35 +5,45 @@
 /// # Examples
 /// ## Example GET request
 /// ```no_run
-/// use psp_net::request;
-/// request! {
+/// # extern crate alloc;
+/// # use psp_net::request;
+/// # use alloc::string::String;
+/// let req = request! {
 ///     "www.example.com" get "/",
-///     "User-Agent" => "Mozilla/5.0",
-/// }
+///     "User-Agent" => "Mozilla/5.0"
+/// };
 /// ```
 ///
 /// ## Example POST request
 /// ```no_run
-/// use psp_net::request;
-/// request! {
+/// # extern crate alloc;
+/// # use psp_net::request;
+/// # use alloc::string::String;
+/// # let body = "test".to_string();
+/// let req = request! {
 ///     "www.example.com" post "/users/create" ContentType::ApplicationJson,
-///     body body,
+///     body body
+/// };
 /// ```
 ///
 /// ## Example With HTTP 1.0
 /// ```no_run
-/// use psp_net::request;
-/// use psp_net::http::HttpVersion;
-/// request! {
+/// # extern crate alloc;
+/// # use psp_net::request;
+/// # use alloc::string::String;
+/// # use psp_net::http::HttpVersion;
+/// let req = request! {
 ///     "www.example.com" get "/"; HttpVersion::V1,
 /// };
 /// ```
 ///
 /// ## Example With Formatted Header
 /// ```no_run
-/// use psp_net::request;
-/// request! {
-///     "www.example.com" get "/"; HttpVersion::V1,
+/// # extern crate alloc;
+/// # use psp_net::request;
+/// # use alloc::string::String;
+/// let req = request! {
+///     "www.example.com" get "/",
 ///     /// enclose the header value in parentheses if it is not
 ///     /// a string, or more specifically a single token tree (tt).
 ///     "User-Agent" => (format!("Mozilla/5.0 ({})", "test")),
@@ -43,11 +53,32 @@
 macro_rules! request {
     (
         $host:tt get $path:tt $(; $http_version:expr)?,
+        $($header:expr => $value:expr,)*
+    ) => {
+        {
+            use alloc::string::{String, ToString};
+            use alloc::vec::Vec;
+            use alloc::vec as a_vec;
+            use psp_net::some_or_none;
+            let http_ver = $crate::some_or_none!($($http_version)?).unwrap_or($crate::http::HttpVersion::V1_1);
+            $crate::_request! {
+                http_version http_ver,
+                host $host,
+                path $path,
+                method $crate::http::Method::Get,
+                auth $crate::http::types::Authorization::None,
+                $($header => $value),*
+            }
+        }
+    };
+
+    (
+        $host:tt get $path:tt $(; $http_version:expr)?,
         $(authorization $auth:expr,)?
         $($header:expr => $value:expr,)*
     ) => {
         {
-            use alloc::{String, string::ToString};
+            use alloc::string::{String, ToString};
             use alloc::vec::Vec;
             use alloc::vec as a_vec;
             use psp_net::some_or_none;
@@ -153,4 +184,69 @@ macro_rules! parse_response {
             httparse::ParserConfig::default().parse_response(&mut res, $response.as_bytes());
         parsed
     }};
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! _request {
+    // no body, no content type
+    (
+        http_version $http_version:expr,
+        host $host:tt,
+        path $path:tt,
+        method $method:tt,
+        auth $auth:expr,
+        $(header $header:tt => $value:tt),*
+    ) => {
+        $crate::http::Request {
+            method: $crate::http::Method::Get,
+            path: $path.to_string(),
+            headers: a_vec![("Host".to_string(), $host.to_string()), $(($header.to_string(), $value.to_string()),)*],
+            authorization: auth,
+            content_type: None,
+            body: Vec::new(),
+            http_version: $http_version,
+        }
+    };
+    // body, no content type
+    (
+        http_version $http_version:expr,
+        host $host:tt,
+        path $path:tt,
+        method $method:tt,
+        auth $auth:expr,
+        $(header $header:tt => $value:tt),*
+        body $body:expr
+    ) => {
+        $crate::http::Request {
+            method: $method,
+            path: $path.to_string(),
+            headers: a_vec![("Host".to_string(), $host.to_string()), $(($header.to_string(), $value.to_string()),)*],
+            authorization: $auth,
+            content_type: None,
+            body: $body,
+            http_version: $http_version,
+        }
+    };
+    // body and content type
+    (
+        http_version $http_version:expr,
+        host $host:tt,
+        path $path:tt,
+        method $method:tt,
+        auth $auth:expr,
+        content_type $content_type:expr,
+        body $body:expr,
+        $(header $header:tt => $value:tt),*
+    ) => {
+        $crate::http::Request {
+            method: $method,
+            path: $path.to_string(),
+            headers: a_vec![("Host".to_string(), $host.to_string()), $(($header.to_string(), $value.to_string()),)*],
+            authorization: $auth,
+            content_type: $content_type,
+            body: $body,
+            http_version: $http_version,
+        }
+    };
 }
