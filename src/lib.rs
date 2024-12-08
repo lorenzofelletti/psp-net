76926bf20aeea83c0020250a38cfa2e2082b1e01
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2024-12-08 15:34:13 +0000

    feat: psp dep optional to allow tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![no_std]
+#![cfg_attr(not(test), no_std)]
 #![feature(trait_alias)]
 #![doc = include_str!("../README.md")]
 #![allow(clippy::cast_sign_loss)]
@@ -9,13 +9,17 @@
 extern crate alloc;
 
 pub mod constants;
+#[cfg(feature = "psp")]
 pub mod dns;
 #[cfg(feature = "http")]
 pub mod http;
 #[cfg(feature = "macros")]
 pub mod macros;
+#[cfg(feature = "psp")]
 pub mod netc;
+#[cfg(feature = "psp")]
 pub mod socket;
 pub mod traits;
 pub mod types;
+#[cfg(feature = "psp")]
 pub mod utils;
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,4 +1,5 @@
 mod socket_flags;
+#[cfg(feature = "psp")]
 mod socket_options;
 
 // re-exports
@@ -6,5 +7,7 @@ pub type Certificate<'a> = embedded_tls::Certificate<'a>;
 
 pub use socket_flags::SocketRecvFlags;
 pub use socket_flags::SocketSendFlags;
+#[cfg(feature = "psp")]
 pub use socket_options::SocketOptions;
+#[cfg(feature = "psp")]
 pub use socket_options::TlsSocketOptions;
